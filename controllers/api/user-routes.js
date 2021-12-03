8a3c3dbd5b2ee4b14bdbedeeadcc40c45d2f0da3
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-03 17:57:33 -0500

    write post route for logout in user-routes.js inside controllers/api folder

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -85,3 +85,13 @@ router.post("/login", async (req, res) => {
     res.json({ user: dbUserData, message: "You are not logged in!" });
   });
 });
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
