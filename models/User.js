cfe7bd06f9674db3be1a9b4f83b8a2f9695e7c73
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-03 15:15:08 -0500

    create columns for user table with id, username, & password in User.js file

--- models/User.js
+++ models/User.js
@@ -7,3 +7,28 @@ class User extends Model {
         return bcrypt.compareSync(loginPw, this.password)
     }
 }
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                len: [5]
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    })
