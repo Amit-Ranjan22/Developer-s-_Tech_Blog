14c0a2f5cf4775667a0a16b422808a2e66dae128
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-03 17:05:25 -0500

    write home get route using async await for all the post data in dashboard-routes.js inside controllers folder

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const withAuth = require("../utils/auth");
+const { User, Post, Comment } = require("../models");
+
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const dbPostData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+    res.render("dashboard", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
