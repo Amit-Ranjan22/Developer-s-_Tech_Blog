243e99e2871bd42c0d7879315b14b7c41d3f9221
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-03 17:56:18 -0500

    write post route for login in user-routes.js inside controllers/api folder

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,3 +58,30 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.post("/login", async (req, res) => {
+  const dbUserData = await User.findOne({
+    where: {
+      username: req.body.username,
+    },
+  });
+  if (!dbUserData) {
+    res.status(400).json({ message: "No user found by this username!" });
+    return;
+  }
+
+  const validatePassword = dbUserData.checkPassword(req.body.password);
+
+  if (!validatePassword) {
+    res.status(400).json({ message: "Incorrect password!" });
+    return;
+  }
+
+  req.session.save(() => {
+    req.session.user_id = dbUserData.id;
+    req.session.username = dbUserData.username;
+    req.session.loggedIn = true;
+
+    res.json({ user: dbUserData, message: "You are not logged in!" });
+  });
+});
