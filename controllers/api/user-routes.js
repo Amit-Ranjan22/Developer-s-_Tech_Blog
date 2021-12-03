85684dcb92e77a08529d184de8e55c9836243a0d
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-03 17:50:51 -0500

    write get route for /:id for indivisual user in user-routes.js inside controllers/api folder

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -9,3 +9,34 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Post,
+          attributes: ["id", "title", "text", "created_at"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "created_at"],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+      ],
+    });
+    if (!dbUserData) {
+      res.status(404).json({ message: "No user found at this id!" });
+      return;
+    }
+    res.json(dbUserData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
