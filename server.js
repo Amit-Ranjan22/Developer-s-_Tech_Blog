352182b1f004eccc1ad94b1f3270e7ae1756e21b
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-03 14:55:43 -0500

    turn on routes and turn on connection to db and server in server.js file

--- server.js
+++ server.js
@@ -26,3 +26,21 @@ const sess = {
         db: sequelize
     })
 };
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
+});
