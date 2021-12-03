55151a5839ad878052334e8f80ddfa686d1da409
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-03 17:10:48 -0500

    write get route for post/:id for individual id in dashboard-routes.js inside controllers folder

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -55,3 +55,44 @@ router.get("/signup", (req, res) => {
   }
   res.render("signup");
 });
+
+router.get("post/:id", async (req, res) => {
+  try {
+    const dbPostData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+    if (!dbPostData) {
+      res.status(404).json({ message: "No post found at this id!" });
+    }
+    const post = dbPostData.get({ plain: true });
+
+    res.render("single-post", {
+      post,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
