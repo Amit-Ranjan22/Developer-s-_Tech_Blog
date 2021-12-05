b799a36202119afd0c308c6f300efa34516dfece
Amitabh Ranjan <amitabh.march22@gmail.com>
2021-12-05 13:21:09 -0500

    write editPostHandler function to edit post in edit-post.js inside public/javascript folder

---
+++ public/javascript/edit-post.js
@@ -0,0 +1,27 @@
+const id = window.location.toString().split("/")[
+  window.location.toString().split("/").length - 1
+];
+
+async function editPostHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector("#post-title").value.trim();
+  const text = document
+    .querySelector('textarea[name="post-text"]')
+    .value.trim();
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title,
+      text,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
+  }
+};
